8c32415a6f4cd55694040138950f5040b5139283
Nadusha <nadusha230275@gmail.com>
2022-07-22 10:38:25 +0500

    работает

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// в одномерном массиве из 123 чисел найти кол-во элементов из отрезка (10,99)
+int[]arr=new int[123];
+for (int i=0; i<arr.Length; i++)
+{
+    arr[i]=new Random().Next(0,100);
+Console.Write($" {arr[i]} ");
+}
+uint c=0;
+for (int i=0; i<arr.Length; i++)
+{
+    if (arr[i]>=10)
+    {
+        c++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"количество элементов {c}");
